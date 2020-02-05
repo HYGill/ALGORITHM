f9ccef7bc3c02645aef6ce9bd6908323a2d45141
HYGill <slslalal12@gmail.com>
2020-02-05 22:58:51 +0900

    Solved Q_컬러링북.java

--- src/PROGRAMMERS/BFS_DFS/Q_컬러링북.java
+++ src/PROGRAMMERS/BFS_DFS/Q_컬러링북.java
@@ -13,14 +13,14 @@ public class Q_컬러링북 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(new InputStreamReader(System.in));
-        m = input.nextInt();
-        n = input.nextInt();
+        n = input.nextInt(); // 4
+        m = input.nextInt(); // 6
 
-        map = new int[m][n];
-        visited = new boolean[m][n];
+        map = new int[n][m];
+        visited = new boolean[n][m];
 
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
                 map[i][j] = input.nextInt();
             }
         }
@@ -31,16 +31,15 @@ public class Q_컬러링북 {
 
         int result = colorCnt[colorCnt.length - 1] + 1; // 처음 0부터 시작했기 때문에 하나 더해준다.
 
-        System.out.println("group : " + groupCnt);
-        System.out.println("cnt : " + result);
+        System.out.println(groupCnt + " " + result);
 
         input.close();
     }
 
     static int groupCnt = 0;
     private static void solution() {
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 if(map[i][j] != 0 && !visited[i][j]){
                     groupCnt++;
                     DFS(i, j);
@@ -58,10 +57,29 @@ public class Q_컬러링북 {
             int dx = x + goX[i];
             int dy = y + goY[i];
 
-            if((-1 < dx && dx < n) && (-1 < dy && dy < n) && map[dx][dy] == map[x][y] && !visited[dx][dy]){
+            if((-1 < dx && dx < n) && (-1 < dy && dy < m) && map[dx][dy] == map[x][y] && !visited[dx][dy]){
                 colorCnt[groupCnt]++;
                 DFS(dx, dy);
             }
         }
     }
 }
+/*
+6 4
+1 1 1 0
+1 2 2 0
+1 0 0 1
+0 0 0 1
+0 0 0 3
+0 0 0 3
+
+6 4
+1 1 1 0
+1 1 1 0
+0 0 0 1
+0 0 0 1
+0 0 0 1
+0 0 0 1
+
+결과 2 6
+ */
