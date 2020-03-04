5a8557f566e795879be44119b440cf71883edec0
HYGill <slslalal12@gmail.com>
2020-03-04 15:18:35 +0900

    solve Q_전화번호목록.java

---
+++ src/PROGRAMMERS/해시/Q_전화번호목록.java
@@ -0,0 +1,33 @@
+package PROGRAMMERS.해시;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Q_전화번호목록 {
+    static Boolean answer = true;
+    public static void main(String[] args) {
+        // String[] phone_book = {"119", "97674223", "1195524421"};
+        String[] phone_book = {"123","456","789"};
+
+        solution(phone_book);
+
+        System.out.println(answer);
+    }
+
+    static public Boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        ArrayList<String> tmp = new ArrayList<>(Arrays.asList(phone_book));
+        int i = 1;
+        for(String s : tmp){
+            if(phone_book[i].contains(s)){
+                answer = false;
+                break;
+            }else if(i == phone_book.length - 1){
+                break;
+            }else
+                i++;
+        }
+
+        return answer;
+    }
+}
