735a64a7d4a5a54748091d14687ee3a3b77fb7e2
HYGill <slslalal12@gmail.com>
2020-03-12 16:45:16 +0900

    solve Q_큰수만들기.java

--- src/PROGRAMMERS/greedy/Q_큰수만들기.java
+++ src/PROGRAMMERS/greedy/Q_큰수만들기.java
@@ -1,56 +1,39 @@
 package PROGRAMMERS.greedy;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class Q_큰수만들기 {
     static String answer = null;
     public static void main(String[] args) {
         String number = "4177252841";
         int k = 4;
 
-        System.out.println(solution(number, k));
+        System.out.println(solution(number, k)); //775841
     }
 
     static public String solution(String number, int k) {
         StringBuilder sb = new StringBuilder();
-        ArrayList<Integer> arrNum = new ArrayList<>();
-        for (int i = 0; i < number.length(); i++) {
-            int tmp = Character.getNumericValue(number.charAt(i));
-            arrNum.add(tmp);
-        }
 
-        ArrayList<Integer> tmp2 = arrNum;
-
-        int answerNum = arrNum.size()-k;
-        int[] result = new int[answerNum];
-        int n = 0;
-
-        while(answerNum > 0){
-            ArrayList<Integer> tmp = new ArrayList<>();
-            for (int i = 0; i < tmp2.size() - answerNum + 1; i++) {
-                tmp.add(arrNum.get(i));
+        int answerNum = number.length() - k; // 몇자리수를 만들어야하는지
+        int in = 0;
+        int start = 0;
+        int i = 0;
+        while(in != number.length() - k){
+            Boolean flag = false;
+            char tmp = number.charAt(start);
+            for (i = start; i < number.length() - answerNum; i++) {
+                if(tmp < number.charAt(i+1)){
+                    tmp = number.charAt(i+1);
+                    start = i+2;
+                    flag = true;
+                }
             }
+            if(!flag)
+                start++;
 
-            int removeSize = 0;
-            while(removeSize <= tmp2.size() - answerNum + 1){
-                arrNum.remove(0);
-                removeSize++;
-            }
-
-            Collections.sort(tmp);
-            result[n] = tmp.get(tmp.size()-1);
-            n++;
-
+            sb.append(tmp);
+            in++;
             answerNum--;
-
-            if(result.length == n)
-                break;
         }
 
-        for (int i = 0; i < result.length; i++) {
-            sb.append(result[i]);
-        }
         answer = sb.toString();
 
         return answer;
