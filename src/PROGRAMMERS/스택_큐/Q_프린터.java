7da63c94588717056c20cf7c263d26e3ea969f8e
HYGill <slslalal12@gmail.com>
2020-03-13 15:19:50 +0900

    solve Q_프린터.java

--- src/PROGRAMMERS/스택_큐/Q_프린터.java
+++ src/PROGRAMMERS/스택_큐/Q_프린터.java
@@ -1,78 +1,43 @@
 package PROGRAMMERS.스택_큐;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Q_프린터 {
     public static void main(String[] args) {
         //int[] priorities = {2,1,3,2};
-        int[] priorities = {1, 1, 9, 1, 1, 1};
-        //int[] priorities = {2,2,2,1,3,4};
+        //int[] priorities = {1, 1, 9, 1, 1, 1};
+        int[] priorities = {2,2,2,1,3,4};
         //int location = 2;//1
-        int location = 0;//5
-        //int location = 3;//6
+        //int location = 0;//5
+        int location = 3;//6
 
         System.out.println(solution(priorities, location));
     }
 
     public static int solution(int[] priorities, int location) {
         int answer = 0;
-        List<Integer> prioritiesList = new ArrayList<>();
-        for (int i = 0; i < priorities.length; i++) {
-            prioritiesList.add(priorities[i]);
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i : priorities){
+            priorityQueue.offer(i);
         }
 
-        List<printInfo> printInfos = new LinkedList<>();
-        for (int i = 0; i < priorities.length; i++) {
-            printInfos.add(new printInfo(i, priorities[i]));
-        }
-
-        prioritiesList.sort(Comparator.reverseOrder()); // max구하기 위함
-        while (printInfos.size() > 0) {
-            for (int i = 0; i < printInfos.size(); i++) {
-                int max = prioritiesList.get(0);
-                int index = FindIndex(printInfos, max); // max인 index구하기
-                if(printInfos.get(0).priority != max) { // max 값까지 갈때까지 앞에꺼 다 뒤로 보내기
-                    for (int j = 0; j < index; j++) {
-                        printInfo tmp = printInfos.get(0);
-                        printInfos.remove(0);
-                        printInfos.add(tmp);
-                    }
-                }else{// max가 맨앞에 있으면
-                    if(printInfos.get(0).index == location){
+        while (!priorityQueue.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorityQueue.peek() == priorities[i]) {
+                    if (i == location) {
                         answer++;
                         return answer;
-                    }else {
-                        prioritiesList.remove(0);
-                        printInfos.remove(0);
+                    } else {
+                        priorityQueue.poll();
                         answer++;
-                        break;
                     }
                 }
             }
         }
 
+
         return answer;
-    }
-
-    public static int FindIndex(List<printInfo> printInfos, int max){
-        int index = 0;
-        for (int i = 0; i < printInfos.size(); i++) {
-            if(printInfos.get(i).priority == max)
-                index = i;
-        }
-        return index;
-    }
-
-    static class printInfo{
-        int index;
-        int priority;
-
-        public printInfo(int index, int priority) {
-            this.index = index;
-            this.priority = priority;
-        }
     }
 }
