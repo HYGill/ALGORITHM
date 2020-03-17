dcef5b128f9f0b649e8e9af3e0684b34b7bb4087
HYGill <slslalal12@gmail.com>
2020-03-17 19:08:44 +0900

    solve Q_네트워크_DFS.java

--- src/PROGRAMMERS/BFS_DFS/Q_네트워크.java
+++ src/PROGRAMMERS/BFS_DFS/Q_네트워크.java
@@ -1,14 +1,19 @@
-package javaTest;
+package PROGRAMMERS.BFS_DFS;
 
 public class Q_네트워크 {
 	static int answer = 0;
 	static boolean[][] visited;
+	static int n;
 
 	public static void main(String[] args) {
-		int[][] numbers = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
-		int n = 3;
+		int[][] numbers = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};//2
+		//int[][] numbers = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};//1
+		//int[][] numbers = {{1,0,0,1},{0,1,1,1},{0,1,1,0},{1,1,0,1}};//1
+		n = 3;
+		//n = 4;
 		System.out.println(solution(n, numbers));
 	}
+
 	public static int solution(int n, int[][] computers) {
 		visited = new boolean[n][n];
 		for (int i = 0; i < n; i++) {
@@ -23,7 +28,7 @@ public class Q_네트워크 {
 	static void dfs(int start, int n, int[][] computers) {
 		for (int i = 0; i < n; i++) {
 			if (computers[start][i] == 1 && !visited[start][i]) {
-				visited[start][i] =visited[i][start] = true;
+				visited[start][i] = visited[i][start] = true;
 				dfs(i, n, computers);
 			}
 		}
