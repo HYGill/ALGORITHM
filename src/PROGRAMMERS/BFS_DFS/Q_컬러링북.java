0119850b5274dcb0d009392a3b80118b8ad9329b
HYGill <slslalal12@gmail.com>
2020-03-18 15:17:39 +0900

    solve Q_컬러링북.java

--- src/PROGRAMMERS/BFS_DFS/Q_컬러링북.java
+++ src/PROGRAMMERS/BFS_DFS/Q_컬러링북.java
@@ -1,65 +1,53 @@
 package PROGRAMMERS.BFS_DFS;
 
-import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Q_컬러링북 {
-    static int m = 0;
-    static int n = 0;
-    static int[][] map;
-    static boolean[][] visited;
-    static List<Integer> colorGroup = new ArrayList<Integer>(); // color 그룹 저장
-    static int[] colorCnt = new int[100]; // 그룹 중 가장 많은 갯수 찾기 위한 배열
-
     public static void main(String[] args) {
-        Scanner input = new Scanner(new InputStreamReader(System.in));
-        n = input.nextInt(); // 4
-        m = input.nextInt(); // 6
-
-        map = new int[n][m];
-        visited = new boolean[n][m];
-
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                map[i][j] = input.nextInt();
-            }
-        }
-
-        solution();
-
-        Arrays.sort(colorCnt);
-
-        int result = colorCnt[colorCnt.length - 1] + 1; // 처음 0부터 시작했기 때문에 하나 더해준다.
-
-        System.out.println(groupCnt + " " + result);
-
-        input.close();
+        int m = 6;
+        int n = 4;
+        //int[][] picture = {{1, 1, 1, 0}, {1, 2, 2, 0}, {1, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 3}, {0, 0, 0, 3}};
+        int[][] picture = {{1, 1, 1, 0}, {1, 1, 1, 0}, {0, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 1}};
+        System.out.println(solution(m, n, picture));
     }
 
-    static int groupCnt = 0;
-    private static void solution() {
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(map[i][j] != 0 && !visited[i][j]){
-                    groupCnt++;
-                    DFS(i, j);
+    static int cnt;
+    static public int[] solution(int m, int n, int[][] picture) {
+        int[] answer = new int[2];
+        List<Integer> numCnt = new ArrayList<>();
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < picture.length; i++) {
+            for (int j = 0; j < picture[i].length; j++) {
+                int now = picture[i][j];
+                if(visited[i][j] == false && now != 0){
+                    visited[i][j] = true;
+                    cnt = 1;
+                    DFS(m, n, picture, visited, i, j, now);
+                    numCnt.add(cnt);
                 }
             }
         }
+
+        Collections.sort(numCnt, Collections.reverseOrder());
+        answer[0] = numCnt.size();
+        answer[1] = numCnt.get(0);
+
+        return answer;
     }
 
-    static int[] goX = {0, 0, -1, 1};
-    static int[] goY = {1, -1, 0, 0};
-    private static void DFS(int x, int y) {
-        visited[x][y] = true;
-
+    static int[] goX = {-1, 0, 1, 0};
+    static int[] goY = {0, 1, 0, -1};
+    private static void DFS(int m, int n, int[][] picture, boolean[][] visited, int x, int y, int now) {
         for(int i = 0; i < 4; i++){
             int dx = x + goX[i];
             int dy = y + goY[i];
 
-            if((-1 < dx && dx < n) && (-1 < dy && dy < m) && map[dx][dy] == map[x][y] && !visited[dx][dy]){
-                colorCnt[groupCnt]++;
-                DFS(dx, dy);
+            if((-1 < dx && dx < m) && (-1 < dy && dy < n) && picture[dx][dy] == now && picture[dx][dy] != 0 && visited[dx][dy] == false){
+                visited[dx][dy] = true;
+                cnt++;
+                DFS(m, n, picture, visited, dx, dy, now);
             }
         }
     }
