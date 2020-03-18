9f47d0338fa743ffcc0c1ad3cf265f756d24b60b
HYGill <slslalal12@gmail.com>
2020-03-18 19:36:10 +0900

    solve Q_2178_미로탐색.java

--- src/BOJ/BFS_DFS/Q_2178_미로탐색.java
+++ src/BOJ/BFS_DFS/Q_2178_미로탐색.java
@@ -1,62 +1,53 @@
 package BOJ.BFS_DFS;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
+// 두 노드 사이의 최단 경로 혹은 임의의 경로 => BFS
 public class Q_2178_미로탐색 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt(); // 4
+        int m = input.nextInt(); // 6
 
-        String[] line = br.readLine().split(" ");
-        int m = Integer.parseInt(line[0]);
-        int n = Integer.parseInt(line[1]);
+        int[][] maze = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
 
-        boolean[][] maze = new boolean[m][n];
-
-        for (int i = 0; i < maze.length; i++) {
-            char[] spite = br.readLine().toCharArray();
-            for (int j = 0; j < spite.length; j++) {
-                maze[i][j] = spite[j] == '1' ? true : false;
+        for(int i = 0; i < n; i++){
+            String str = input.next();
+            for(int j = 0; j < m; j++){
+                maze[i][j] = Integer.parseInt(str.charAt(j)+"");
             }
         }
 
-        boolean[][] visited = new boolean[m][n];
-        int[][] result = new int[m][n];
+        BFS(maze, visited, m, n, 0, 0);
 
-        Queue dx = new LinkedList<>();
-        Queue dy = new LinkedList<>();
-
-        visited[0][0] = true;
-        dx.add(0);
-        dy.add(0);
-        result[0][0] = 1;
-
-        while(!dx.isEmpty()) {
-            int x = (int) dx.poll(); // 가장 먼저 보관한 값 꺼내고 반환, 반환하는 것
-            int y = (int) dy.poll();
-            if(y > 0 && maze[y - 1][x] && visited[y - 1][x] == false) {
-                plus(visited, dx, dy, result, x, y - 1, result[y][x]);
-            }
-            if(y < maze.length - 1 && maze[y + 1][x] && visited[y + 1][x] == false) {
-                plus(visited, dx, dy, result, x, y + 1, result[y][x]);
-            }
-            if(x > 0 && maze[y][x - 1] && visited[y][x - 1] == false) {
-                plus(visited, dx, dy, result, x - 1, y, result[y][x]);
-            }
-            if(x < maze[0].length - 1 && maze[y][x + 1] && visited[y][x + 1] == false) {
-                plus(visited, dx, dy, result, x + 1, y, result[y][x]);
-            }
-        }
-        System.out.println(result[m-1][n-1]);
+        System.out.println(maze[n-1][m-1]);
     }
 
-    public static void plus(boolean[][] visited, Queue xs, Queue ys, int[][] result, int x, int y, int prevNode) {
-        visited[y][x] = true;
-        xs.add(x);
-        ys.add(y);
-        result[y][x] = prevNode + 1;
+    static int[] goX = {0, 1, 0, -1}; // 우 좌
+    static int[] goY = {1, 0, -1, 0}; // 하 상
+    static public void BFS(int[][] maze, boolean[][] visited, int m, int n, int x, int y){
+        Queue xQ = new LinkedList();
+        Queue yQ = new LinkedList();
+        xQ.add(x); yQ.add(y);
+        visited[x][y] = true;
+
+        while (!xQ.isEmpty() && !yQ.isEmpty()){
+            x = (int) xQ.poll();
+            y = (int) yQ.poll();
+            for (int i = 0; i < 4; i++) {
+                int dx = x + goX[i];
+                int dy = y + goY[i];
+
+                if((-1 < dx && dx < n) && (-1 < dy && dy < m) && maze[dx][dy] == 1 && visited[dx][dy] == false){
+                    xQ.add(dx); yQ.add(dy);
+                    maze[dx][dy] = maze[x][y] + 1;
+                    visited[dx][dy] = true;
+                }
+            }
+        }
     }
 }
