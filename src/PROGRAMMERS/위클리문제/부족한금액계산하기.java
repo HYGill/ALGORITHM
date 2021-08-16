44099604a1c96ae5613dae9adf3794a4a4561ba7
= <=>
2021-08-16 15:12:53 +0900

    unsolved Q_부족한금액계산하기

---
+++ src/PROGRAMMERS/위클리문제/부족한금액계산하기.java
@@ -0,0 +1,13 @@
+package PROGRAMMERS.위클리문제;
+
+public class 부족한금액계산하기 {
+    public static void main(String[] args) {
+        int price = 3;
+        int money = 20;
+        int count = 4;
+
+        long answer = 0;
+
+        System.out.println(answer);
+    }
+}
