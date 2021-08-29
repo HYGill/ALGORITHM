cdf2c5d2406116f69cc297aaf3277fe6a5758a60
HYGill <slslalal12@gmail.com>
2021-08-30 06:34:01 +0900

    solve Q_여행경로_2021.java

--- src/PROGRAMMERS/BFS_DFS/Q_여행경로_2021.java
+++ src/PROGRAMMERS/BFS_DFS/Q_여행경로_2021.java
@@ -1,45 +1,39 @@
 package PROGRAMMERS.BFS_DFS;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
-import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Q_여행경로_2021 {
+    private static boolean[] visited;
+    private static List<String> answers;
+
     public static void main(String[] args) {
-      
         String[][] tickets = {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}}; 
-     
-        System.out.println(solution(tickets));
-    }
 
-    private static String[] solution(String[][] tickets){
-        String[] answer = {};
-        String start = "ICN";
+        visited = new boolean[tickets.length];    
+        answers = new ArrayList<String>();
+        int count = 0;
+        dfs(count, "ICN", "ICN",tickets);                
+        Collections.sort(answers);
 
-        bfs(start, tickets);
-
-        return answer;
-    }
-
-    private static void bfs(String start, String[][] tickets){
-        Queue<String> q = queueSetting(start, tickets);
-        boolean[] visited = new boolean[tickets.length];
-
-        // 알파벳순으로 앞서는 로직 추가
-        // q.polll 후 이 전것보다 알파벳이 앞에 있을 시 visited 표시 후 넘어감
+        String[] answer = answers.get(0).split(",");
 
     }
 
-    private static Queue<String> queueSetting(String start, String[][] tickets){
-        Queue<String> q = new LinkedList<>();
-        // start으로 시작하는 배열들의 뒷자리를 모두 Queue에 추가
-        for(int i = 0; i < tickets.length; i++){
-            if(tickets[i][0].equals(start)){
-                q.offer(tickets[i][1]);
-            }
+    static public void dfs(int count, String nowAirport, String answer, String[][]ticktes) {
+        if(count == ticktes.length) {
+            answers.add(answer);
+            return;
         }
 
-        return q;
+        for(int i = 0; i < ticktes.length; i++) {
+            if(!visited[i] && ticktes[i][0].equals(nowAirport)) {
+                visited[i] = true;
+                dfs(count+1, ticktes[i][1],answer+","+ticktes[i][1] , ticktes);
+                visited[i] = false;
+            }
+        }
+        return;
     }
 }
