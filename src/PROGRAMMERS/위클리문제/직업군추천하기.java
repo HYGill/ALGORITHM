e8334b795e718338ea85a281ae2b7973e582de87
HYGill <slslalal12@gmail.com>
2021-09-10 06:35:59 +0900

    solve Q_직업군추천하기.java

--- src/PROGRAMMERS/위클리문제/직업군추천하기.java
+++ src/PROGRAMMERS/위클리문제/직업군추천하기.java
@@ -1,7 +1,9 @@
 package PROGRAMMERS.위클리문제;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Collections;
@@ -11,25 +13,28 @@ public class 직업군추천하기 {
         // 직업군 언어 점수
         String[] table = {"SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"};
         // 개발자가 사용하는 언어
-        String[] languages = {"PYTHON", "C++", "SQL"};
+        String[] languages = {"JAVA", "JAVASCRIPT"};
         // 언어 선호도
-        int[] preference = {7, 5, 5};
+        int[] preference = {7, 5};
 
         System.out.println(solution(table, languages, preference));
     }
 
     public static String solution(String[] table, String[] languages, int[] preference) {
+        String answer = null;
         List<Integer> result = new ArrayList<>();
 
-        Map<String, List<String>> tableMap = new HashMap<>();
+        Map<String, List<String>> tableMap = new LinkedHashMap<>();
+        List<String> orderMap = new ArrayList<>();
         for(int i = 0; i < table.length; i++){
             String[] tmp = table[i].split(" ");
 
             List<String> saveList = new ArrayList<>();
-            for(int j = 1; j < tmp.size(); j++){
+            for(int j = 1; j < tmp.length; j++){
                 saveList.add(tmp[j]);
             }
             tableMap.put(tmp[0], saveList);
+            orderMap.add(tmp[0]);
         }
 
         Map<String, Integer> scoreMap = new HashMap<>();
@@ -51,10 +56,25 @@ public class 직업군추천하기 {
         }
         
         // result 중 가장 높은 점수 찾고
-        int resultMax = Collections.max(list);
+        int resultMax = Collections.max(result);
         int index = result.indexOf(resultMax);
-        
+
+        List<String> sameScore = new ArrayList<>();
+        for(int i = 0; i < result.size(); i++){
+            if(result.get(i) == resultMax){
+                sameScore.add(orderMap.get(i));
+            }
+        }
+
         // 그것에 해당하는 tableMap key 값 return
-        return tableMap.get(index);
+        // 동일한 것이 있으면 사전 순
+        if(sameScore.size() == 1){
+            answer = orderMap.get(index);
+        }else{
+            Collections.sort(sameScore);
+            answer = sameScore.get(0);
+        }
+        
+        return answer;
     }
 }
