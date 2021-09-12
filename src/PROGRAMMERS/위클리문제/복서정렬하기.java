7f8518772a8a59d61720ba3e7cc679ca31ffea50
HYGill <slslalal12@gmail.com>
2021-09-13 06:44:45 +0900

    unsolve Q_복서정렬하기.java

--- src/PROGRAMMERS/위클리문제/복서정렬하기.java
+++ src/PROGRAMMERS/위클리문제/복서정렬하기.java
@@ -1,7 +1,11 @@
 package PROGRAMMERS.위클리문제;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class 복서정렬하기 {
     public static void main(String[] args) {
@@ -19,10 +23,14 @@ public class 복서정렬하기 {
         // head2head String 자르고 승률계산
         for(int i = 0; i < weights.length; i++){
             float percent = 0f;
+            int cnt = 0;
             for(int j = 0; j < head2head[i].length(); j++){
                 // 검사하는 글자가 W이면 count
+                if(head2head[i].charAt(j) == 'W')
+                    cnt++;
             }
             // 승률계산
+            percent = (float)cnt / (float)(head2head[i].length()-1);
 
             if(winRate.contains(percent)){
                 flag = true;
@@ -32,11 +40,21 @@ public class 복서정렬하기 {
 
         // 승률이 같은게 있다
         if(flag){
-            // 몸무게 무거운 사람 이긴 count가 더 많을 것 반별
-
-            // count가 높으면 몸무게 비교
+            // 몸무게 무거운 사람 이긴 count가 더 많은 것 판별
+            
+            // 그것도 같으면 몸무게 비교
         }else{
             // 승률 높은 것부터 return
+            Map<Float, Integer> compareMap = new HashMap<>();
+            for(int i = 0; i < weights.length; i++){
+                compareMap.put(winRate.get(i), weights[i]);
+            }
+
+            Collections.reverse(winRate);
+
+            for(int i = 0; i < weights.length; i++){
+                answer[i] = compareMap.get(winRate.get(i));
+            }
         }
 
         return answer;
