d018305323de35cc48da841537aa556cdf088a42
HYGill <slslalal12@gmail.com>
2021-09-14 06:56:18 +0900

    unsolve Q_입실퇴실.java

---
+++ src/PROGRAMMERS/위클리문제/입실퇴실.java
@@ -0,0 +1,14 @@
+package PROGRAMMERS.위클리문제;
+
+public class 입실퇴실 {
+    public static void main(String[] args) {
+        int[] enter = {1,3,2};
+        int[] leave = {1,2,3};
+        System.out.println(solution(enter, leave));
+    }
+
+    public static int[] solution(int[] enter, int[] leave) {
+        int[] answer = {};
+        return answer;
+    }
+}
