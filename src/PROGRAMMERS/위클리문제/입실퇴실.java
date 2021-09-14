9bbe04a89093186f46b5a6dedb1e4b0f07cdfb37
HaeYoung Gill <47243329+HYGill@users.noreply.github.com>
2021-09-14 17:09:53 +0900

    Update 입실퇴실.java

--- src/PROGRAMMERS/위클리문제/입실퇴실.java
+++ src/PROGRAMMERS/위클리문제/입실퇴실.java
@@ -1,6 +1,7 @@
 package PROGRAMMERS.위클리문제;
 
 public class 입실퇴실 {
+    private static List<Integer> leaveList = new ArrayList<>();
     public static void main(String[] args) {
         int[] enter = {1,3,2};
         int[] leave = {1,2,3};
@@ -11,28 +12,31 @@ public class 입실퇴실 {
         int[] answer = {};
         
         List<Integer> enterList = Arrays.asList(enter);
-        List<Integer> leaveList = Arrays.asList(leave);
+        leaveList = Arrays.asList(leave);
         List<Integer> room = new ArrayList<>();
         Map<Integer, Integer> meetCnt = new LinkedHashMap<>();
 
         while(enterList.size() > 0 && leaveList.size() > 0 && room.size() > 0){
+            int nowEnter = enterList.get(0);
             // enter가 room에 들어오고 enterList에서 제거
             room.add(enterList.get(0));
             enterList.remove(0);
             
-            // 점수 계산
-            for(int i : room){
-                int tmp = meetCnt.get(i);
-                tmp++;
-                meetCnt.put(i, tmp);
+            if(room.size() > 1){
+                // 점수 계산. 새로 들어온사람은 기존 인원수 만큼 cnt증가
+                for(Integer key : map.keySet()) {
+                    int tmp = map.get(key);
+                    if(key == nowEnter){
+                        tmp += room.size()-1;
+                    }else{
+                        tmp++;
+                    }
+                    meetCnt.put(key, tmp);
+                }
             }
             
-            // leaveList.get(0) 과 같은지 비교
-            // 같으면 room에서 빼고 leaveList에서도 빼기
-            if(room.get(0) == leaveList.get(0)){
-                room.remove(0);
-                leaveList.remove(0);
-            }
+            // room 떠날 사람 검사
+            removeRecursion(room, leaveList.get(0));
         }
 
         int index = 0;
@@ -40,5 +44,19 @@ public class 입실퇴실 {
             answer[index] = map.get(key);
         }
         return answer;
+    }
+    
+    public static void removeRecursion(List<Integer> room, int removeNum){
+        // leaveList.get(0)이 room에 있는지 확인
+        // 같으면 room에서 빼고 leaveList에서도 빼기
+        if(room.contains(removeNum)){
+                int index = room.indexOf(removeNum);
+                room.remove(index);
+                leaveList.remove(0);
+        }
+        
+        // 방을 떠날 수 있는 사람이 뒤에 있는지 확인
+        if(room.contains(leaveList.get(0))
+           removeRecursion(room, leaveList.get(0))
     }
 }
