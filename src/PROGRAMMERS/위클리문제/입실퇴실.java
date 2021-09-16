af072460e2a2b4f3018e777e73c8d85eb1a92d99
HYGill <slslalal12@gmail.com>
2021-09-16 12:36:58 +0900

    unsolve Q_입실퇴실.java

--- src/PROGRAMMERS/위클리문제/입실퇴실.java
+++ src/PROGRAMMERS/위클리문제/입실퇴실.java
@@ -1,5 +1,10 @@
 package PROGRAMMERS.위클리문제;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class 입실퇴실 {
     private static List<Integer> leaveList = new ArrayList<>();
     public static void main(String[] args) {
@@ -11,12 +16,14 @@ public class 입실퇴실 {
     public static int[] solution(int[] enter, int[] leave) {
         int[] answer = {};
         
-        List<Integer> enterList = Arrays.asList(enter);
-        leaveList = Arrays.asList(leave);
+        List<Integer> enterList = new ArrayList<Integer>();
+        for(int i : enter) enterList.add(i);
+        for(int i : leave) leaveList.add(i);
+        
         List<Integer> room = new ArrayList<>();
         Map<Integer, Integer> meetCnt = new LinkedHashMap<>();
 
-        while(enterList.size() > 0 && leaveList.size() > 0 && room.size() > 0){
+        while(enterList.size() > 0 && leaveList.size() > 0){
             int nowEnter = enterList.get(0);
             // enter가 room에 들어오고 enterList에서 제거
             room.add(enterList.get(0));
@@ -24,8 +31,8 @@ public class 입실퇴실 {
             
             if(room.size() > 1){
                 // 점수 계산. 새로 들어온사람은 기존 인원수 만큼 cnt증가
-                for(Integer key : map.keySet()) {
-                    int tmp = map.get(key);
+                for(Integer key : meetCnt.keySet()) {
+                    int tmp = meetCnt.get(key);
                     if(key == nowEnter){
                         tmp += room.size()-1;
                     }else{
@@ -40,8 +47,8 @@ public class 입실퇴실 {
         }
 
         int index = 0;
-        for(Integer key : map.keySet()) {
-            answer[index] = map.get(key);
+        for(Integer key : meetCnt.keySet()) {
+            answer[index] = meetCnt.get(key);
         }
         return answer;
     }
@@ -56,7 +63,8 @@ public class 입실퇴실 {
         }
         
         // 방을 떠날 수 있는 사람이 뒤에 있는지 확인
-        if(room.contains(leaveList.get(0))
-           removeRecursion(room, leaveList.get(0))
+        if(room.contains(leaveList.get(0))){
+           removeRecursion(room, leaveList.get(0));
+        }
     }
 }
