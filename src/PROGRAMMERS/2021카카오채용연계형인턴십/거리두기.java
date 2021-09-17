51da7f89d9fd79d94443ccb80750069eac363ac5
HaeYoung Gill <47243329+HYGill@users.noreply.github.com>
2021-09-17 14:44:18 +0900

    Update 거리두기.java

--- src/PROGRAMMERS/2021카카오채용연계형인턴십/거리두기.java
+++ src/PROGRAMMERS/2021카카오채용연계형인턴십/거리두기.java
@@ -11,8 +11,29 @@ class Solution {
   public int[] solution(String[][] places) {
     int[] answer = {};
     
-    
+    for(int i = 0; i < places.length(); i++){
+      // places의 한 row씩 검사
+      String[][] splitPlaces = new String[places.length()][places[i][0].length()];
+      
+      for(int j = 0; j < places[i].length(); j++){
+        for(int k = 0; k < places[i][j].length(); k++){
+          String nowChar = places[i][j].getChar(k);
+          
+          if(nowChar.equals("P")){
+            // P나오면 다음 P나올때까지 거리 2 이하일때만 검사
+            
+          }
+          
+          splitPlaces[j][k] = nowChar;
+        }
+      }
+    }
     
     return answer;
+  }
+  
+  // bfs
+  public static void distance(String splitPlaces[][], int j, int k){
+    
   }
 }
