f2949937c844a2ed7b8d880db800a36830cd8675
HaeYoung Gill <47243329+HYGill@users.noreply.github.com>
2021-09-17 14:12:04 +0900

    unsolved 거리두기.java

---
+++ src/PROGRAMMERS/2021카카오채용연계형인턴십/거리두기.java
@@ -0,0 +1,18 @@
+class Solution {
+  public static void main(String[] args) {
+    String[][] places = {{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"}, {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"}, 
+                         {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"}, {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"}, 
+                         {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}};
+    
+    System.out.println(solution(places));
+  }
+
+  
+  public int[] solution(String[][] places) {
+    int[] answer = {};
+    
+    
+    
+    return answer;
+  }
+}
