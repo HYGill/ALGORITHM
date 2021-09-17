17a7579b557bb3fa088a345d3e2b424944d3109a
HaeYoung Gill <47243329+HYGill@users.noreply.github.com>
2021-09-17 16:16:29 +0900

    Update 거리두기.java

--- src/PROGRAMMERS/2021카카오채용연계형인턴십/거리두기.java
+++ src/PROGRAMMERS/2021카카오채용연계형인턴십/거리두기.java
@@ -11,28 +11,20 @@ class Solution {
   public int[] solution(String[][] places) {
     int[] answer = {};
     
+    List<Integer[]> people = new ArrayList<>();
     for(int i = 0; i < places.length(); i++){
-      // places의 한 row씩 검사
-      String[][] splitPlaces = new String[places.length()][places[i][0].length()];
-      
       for(int j = 0; j < places[i].length(); j++){
-        for(int k = 0; k < places[i][j].length(); k++){
-          String nowChar = places[i][j].getChar(k);
-          
-          if(nowChar.equals("P")){
-            // P나오면 다음 P나올때까지 거리 2 이하일때만 검사
-            
-          }
-          
-          splitPlaces[j][k] = nowChar;
-        }
+        if(places[i].charAt(j) == 'P')
+          people.add([i,j]);
       }
     }
+    
+    // people에 저장한 것 거리 계산
     
     return answer;
   }
   
-  // bfs
+  // 거리계산 함수
   public static void distance(String splitPlaces[][], int j, int k){
     
   }
