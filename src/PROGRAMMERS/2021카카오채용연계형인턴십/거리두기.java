ada83302b3432685952c8910279aacb059140160
HYGill <slslalal12@gmail.com>
2021-09-23 06:52:05 +0900

    unsolve Q_거리두기.java

--- src/PROGRAMMERS/2021카카오채용연계형인턴십/거리두기.java
+++ src/PROGRAMMERS/2021카카오채용연계형인턴십/거리두기.java
@@ -10,40 +10,57 @@ class Solution {
     System.out.println(solution(places));
   }
 
-  
   public static int[] solution(String[][] places) {
     int[] answer = new int[places.length];
     
-    List<Integer[]> people = new ArrayList<>();
     for(int i = 0; i < places.length; i++){
+      List<Integer[]> people = new ArrayList<>();
       for(int j = 0; j < places[i].length; j++){
         for(int k = 0; k < places[i][j].length(); k++){
           if(places[i][j].charAt(k) == 'P'){
-            Integer[] tmp = {i,j};
+            Integer[] tmp = {j,k};
             people.add(tmp);
           }
         }
       }
-      // people에 저장한 것 거리 계산
-      answer[i] = distance(people);
+      // people에 저장한 거리 기준으로 지키면 1, 안지키면 0
+      answer[i] = calcul(people, places[i]);
     }
     
     return answer;
   }
   
   // 거리계산 함수
-  // 두 테이블 T1, T2가 행렬 (r1, c1), (r2, c2)에 각각 위치하고 있다면, T1, T2 사이의 맨해튼 거리는 |r1 - r2| + |c1 - c2| 입니다
-  public static int distance(List<Integer[]> people){
+  // 두 테이블 T1, T2가 행렬 (r1, c1), (r2, c2)에 각각 위치하고 있다면, T1, T2 사이의 맨해튼 거리는 |r1 - r2| + |c1 - c2|
+  public static int calcul(List<Integer[]> people, String[] place){
     for(int i = 0; i < people.size() - 1 ; i++){
       Integer[] spot1 = people.get(i);
       Integer[] spot2 = people.get(i+1);
 
-      float distance = Math.abs(spot1[0] - spot2[0]) + Math.abs(spot1[1] - spot2[1]);
+      int distance = Math.abs(spot1[0] - spot2[0]) + Math.abs(spot1[1] - spot2[1]);
 
-      if(distance > 2) {
-        return 0;
+      if(distance <= 2) {
+        boolean flag = false;
+        // 파티션 검사
+        // 일렬로 있으면
+        if(spot1[0] == spot2[0]){
+          for(int j = spot1[1]; j < spot2[1]; j++){
+            if(place[spot1[0]].charAt(j) == 'X'){
+              flag = true;
+              break;
+            }
+          }
+        }
+        // if(사람이 대각선에 위치하면)
+        else if((spot1[0] + 1 == spot2[0]) && (spot1[1] == spot2[1] + 1)){
+
+        }
+
+        if(!flag)
+          return 0;
       }
     }
+
     return 1;
   }
 }
