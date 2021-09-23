fc9564d98c5e101f77e05e75617e1634b778a340
HYGill <slslalal12@gmail.com>
2021-09-24 06:58:42 +0900

    unsolve Q_거리두기.java

--- src/PROGRAMMERS/2021카카오채용연계형인턴십/거리두기.java
+++ src/PROGRAMMERS/2021카카오채용연계형인턴십/거리두기.java
@@ -35,10 +35,10 @@ class Solution {
   public static int calcul(List<Integer[]> people, String[] place){
     for(int i = 0; i < people.size() - 1 ; i++){
       Integer[] nowSpot = people.get(i);
-      // Integer[] spot2 = people.get(i+1); 
       
       // i+1 부터 모두 검사. distance와 대각선, 파티션 유무
-      for(Integer[] compareSpot : people){
+      for(int k = i+1; k < people.size() - 1; k++){
+        Integer[] compareSpot = people.get(k);
         int distance = Math.abs(nowSpot[0] - compareSpot[0]) + Math.abs(nowSpot[1] - compareSpot[1]);
         
         if(distance <= 2) {
@@ -51,49 +51,30 @@ class Solution {
               }
             }
           }
-          // if(사람이 대각선에 위치하면)
-          else if((nowSpot[0] + 1 == compareSpot[0]) && (nowSpot[1] == compareSpot[1] + 1)){
-            if(place[nowSpot[0]].charAt(nowSpot[1]+1) == 'X' && place[compareSpot[0]].charAt(compareSpot[1]-1) == 'X'){
+          // if(세로위치)
+          else if(nowSpot[1] == compareSpot[1]){
+            if(place[nowSpot[0]+1].charAt(nowSpot[1]) == 'X'){
               flag = true;
             }
-          }else if((nowSpot[0] + 1 == compareSpot[0]) && (nowSpot[1] + 1 == compareSpot[1]){
+          }
+          // if(사람이 대각선에 위치하면)
+          else if((nowSpot[0] + 1 == compareSpot[0]) && (nowSpot[1] == compareSpot[1] + 1)){
             if(place[nowSpot[0]].charAt(nowSpot[1]-1) == 'X' && place[compareSpot[0]].charAt(compareSpot[1]+1) == 'X'){
               flag = true;
             }
           }
-        }else{
-          // 거리가 2가 넘는 것이 있다면 인접한거 다 검사한거여서 다음으로 넘어감
-          break;
-        }
-                   
-        if(!flag)
-          return 0;
-      }
-/*
-      int distance = Math.abs(spot1[0] - spot2[0]) + Math.abs(spot1[1] - spot2[1]);
-
-      if(distance <= 2) {
-        boolean flag = false;
-        // 파티션 검사
-        // 일렬로 있으면
-        if(spot1[0] == spot2[0]){
-          for(int j = spot1[1]; j < spot2[1]; j++){
-            if(place[spot1[0]].charAt(j) == 'X'){
+          else if((nowSpot[0] + 1 == compareSpot[0]) && (nowSpot[1] + 1 == compareSpot[1])){
+            if(place[nowSpot[0]].charAt(nowSpot[1]+1) == 'X' && place[compareSpot[0]].charAt(compareSpot[1]-1) == 'X'){
               flag = true;
-              break;
             }
           }
-        }
-        // if(사람이 대각선에 위치하면)
-        else if((spot1[0] + 1 == spot2[0]) && (spot1[1] == spot2[1] + 1)){
 
+          if(!flag)
+            return 0;
+            
         }
-
-        if(!flag)
-          return 0;
       }
     }
-*/
     return 1;
   }
 }
