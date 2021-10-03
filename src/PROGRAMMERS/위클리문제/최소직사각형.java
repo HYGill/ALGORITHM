1983ce43d6f38fe7d105434c5e72028b269fdf99
HYGill <slslalal12@gmail.com>
2021-10-03 12:56:34 +0900

    solve Q_최소직사각형

--- src/PROGRAMMERS/위클리문제/최소직사각형.java
+++ src/PROGRAMMERS/위클리문제/최소직사각형.java
@@ -10,11 +10,11 @@ public class 최소직사각형 {
     public static int solution(int[][] sizes) {
         int answer = 0;
 
-        int originBig = (size[0][0] >= sizes[0][1]) ? sizes[0][0] : sizes[0][1];
-        int originSmall = (size[0][0] <= sizes[0][1]) ? sizes[0][0] : sizes[0][1];
+        int originBig = (sizes[0][0] >= sizes[0][1]) ? sizes[0][0] : sizes[0][1];
+        int originSmall = (sizes[0][0] <= sizes[0][1]) ? sizes[0][0] : sizes[0][1];
         for(int i = 1; i < sizes.length; i++){
-            int bigSize = (size[i][0] >= sizes[i][1]) ? sizes[i][0] : sizes[i][1];
-            int smallSize = (size[i][0] <= sizes[i][1]) ? sizes[i][0] : sizes[i][1];
+            int bigSize = (sizes[i][0] >= sizes[i][1]) ? sizes[i][0] : sizes[i][1];
+            int smallSize = (sizes[i][0] <= sizes[i][1]) ? sizes[i][0] : sizes[i][1];
  
             if(bigSize > originBig){
                 originBig = bigSize;
