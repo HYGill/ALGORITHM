1fbedf71ba15a76939045713655348d5c22be225
HYGill <slslalal12@gmail.com>
2021-10-05 06:27:23 +0900

    solve Q_최소직사각형.java

--- src/PROGRAMMERS/위클리문제/최소직사각형.java
+++ src/PROGRAMMERS/위클리문제/최소직사각형.java
@@ -1,5 +1,3 @@
-package PROGRAMMERS.위클리문제;
-
 public class 최소직사각형 {
     public static void main(String[] args) {
         int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
@@ -8,8 +6,6 @@ public class 최소직사각형 {
     }
 
     public static int solution(int[][] sizes) {
-        int answer = 0;
-
         int originBig = (sizes[0][0] >= sizes[0][1]) ? sizes[0][0] : sizes[0][1];
         int originSmall = (sizes[0][0] <= sizes[0][1]) ? sizes[0][0] : sizes[0][1];
         for(int i = 1; i < sizes.length; i++){
