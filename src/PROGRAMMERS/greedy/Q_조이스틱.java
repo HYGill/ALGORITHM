72a8c3a193ac49f0f4bad97322ce5e263ec4a4ae
HYGill <slslalal12@gmail.com>
2021-10-05 06:25:34 +0900

    unsolve Q_조이스틱.java

---
+++ src/PROGRAMMERS/greedy/Q_조이스틱.java
@@ -0,0 +1,16 @@
+public class Q_조이스틱 {
+    public static void main(String[] args) {
+        String name = "JEROEN";
+        
+        System.out.println(solution(name));
+    }
+
+    public static int solution(String name) {
+        int answer = 0;
+
+        // AAAAAA(글자 수 만큼으로 시작)
+        // ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+        return answer;
+    }
+}
