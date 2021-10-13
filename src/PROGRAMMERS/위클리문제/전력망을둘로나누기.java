314f165df96efd03f6d64bb47ff96f239183caac
HYGill <slslalal12@gmail.com>
2021-10-14 06:40:11 +0900

    unsolved Q_전력망을둘로나누기.java

---
+++ src/PROGRAMMERS/위클리문제/전력망을둘로나누기.java
@@ -0,0 +1,20 @@
+public class 전력망을둘로나누기 {
+    public static void main(String[] args) {
+        int n = 9;
+        int[][] wires = {{1,3},{2,3},{3,4},{4,5},{4,6},{4,7},{7,8},{7,9}}; 
+        
+        System.out.println(solution(n, wires));
+    }
+
+    public static int solution(int n, int[][] wires) {
+        int answer = -1;
+
+        /*
+        * dfs로 접근
+        * 
+        */ 
+
+
+        return answer;
+    }
+}
