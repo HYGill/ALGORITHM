2d23af4f9320cb12b6c01a4fade50b8d2508ee32
HaeYoung Gill <47243329+HYGill@users.noreply.github.com>
2021-11-29 14:22:51 +0900

    Update 피로도.java

--- src/PROGRAMMERS/위클리문제/피로도.java
+++ src/PROGRAMMERS/위클리문제/피로도.java
@@ -1,3 +1,28 @@
 public class 피로도 {
+    public static void main(String[] args) {
+        int k = 80;
+        int[][] dungeons = {{80,20},{50,40},{30,10}};
+        
+        solution(k, dungeons);
+    }
     
+    public static int solution(int k, int[][] dungeons) {
+        int answer = 0;
+        boolean[] visited = new boolean[dungeons.length];
+        
+        // dfs
+        dfs(dungeons, k, answer);
+        
+        return answer;
+    }
+    
+    public static void dfs(int[][] dungeons, int nowPotion, int cnt){
+        for(int i = 0; i < dungeons.length; i++){
+            if(!visited[i]){
+                visited[i] = true
+                dfs(dungeons, nowPotion-dungeons[i][1], cnt+1);
+                visited[i] = false;
+            }
+        }
+    }
 }
