efc13fe2d2a061470b4038a742ad95b319cc0598
= <=>
2021-11-30 14:08:15 +0900

    solve Q_피로도.java

--- src/PROGRAMMERS/위클리문제/피로도.java
+++ src/PROGRAMMERS/위클리문제/피로도.java
@@ -1,16 +1,15 @@
 public class 피로도 {
+    static boolean[] visited;
+    static int answer = 0;
     public static void main(String[] args) {
         int k = 80;
         int[][] dungeons = {{80,20},{50,40},{30,10}};
         
-        solution(k, dungeons);
+        System.out.println(solution(k, dungeons));
     }
     
     public static int solution(int k, int[][] dungeons) {
-        int answer = 0;
-        boolean[] visited = new boolean[dungeons.length];
-        
-        // dfs
+        visited = new boolean[dungeons.length];
         dfs(dungeons, k, answer);
         
         return answer;
@@ -18,9 +17,14 @@ public class 피로도 {
     
     public static void dfs(int[][] dungeons, int nowPotion, int cnt){
         for(int i = 0; i < dungeons.length; i++){
+            if(cnt > answer)
+                answer = cnt;
             if(!visited[i]){
-                visited[i] = true
-                dfs(dungeons, nowPotion-dungeons[i][1], cnt+1);
+                visited[i] = true;
+                      
+                if(nowPotion >= dungeons[i][0]){
+                    dfs(dungeons, nowPotion-dungeons[i][1], cnt+1);
+                }
                 visited[i] = false;
             }
         }
