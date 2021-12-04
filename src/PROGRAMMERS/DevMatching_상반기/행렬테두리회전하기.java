2b1956bed55d9f7971633385948ad16ba9db49a5
= <=>
2021-12-04 15:03:57 +0900

    unsolve Q_행렬테두리회전하기.java

---
+++ src/PROGRAMMERS/DevMatching_상반기/행렬테두리회전하기.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class 행렬테두리회전하기 {
+    static int answer = 0;
+    static int startCnt = 0;
+    public static void main(String[] args) {
+        int rows = 6;
+        int columns = 6;
+        int[][] queries = {{2,2,5,4},{3,3,6,6},{5,1,6,3}};
+        
+        System.out.println(solution(rows, columns, queries));
+    }
+
+    public static int solution(int rows, int columns, int[][] queries) {
+
+        return answer;
+    }
+
+}
