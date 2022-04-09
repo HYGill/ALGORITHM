4a2011daa2f4e77065db362dddff2b0c7279aaba
= <slslalal12@gmail.com>
2022-04-09 15:10:56 +0900

    solved Q_베스트앨범.java

--- src/PROGRAMMERS/해시/Q_베스트앨범.java
+++ src/PROGRAMMERS/해시/Q_베스트앨범.java
@@ -1,83 +1,84 @@
-package PROGRAMMERS.해시;
 
 import java.util.*;
 
 public class Q_베스트앨범 {
-    static Integer[] answer = {};
     public static void main(String[] args) {
         String[] genres = {"classic", "pop", "classic", "classic", "pop"};
         int[] plays = {500, 600, 150, 800, 2500};
 
-        solution(genres, plays);
+        int[] answer = solution(genres, plays);
+        for(int i : answer){
+            System.out.println(i);
+        }
     }
-
-    static public int[] solution(String[] genres, int[] plays) {
-        List<Integer> result = new ArrayList<>();
-        // 장르 정렬
-        HashMap<String, Integer> genresHash = new HashMap<>();
-        for(int i = 0; i < genres.length; i++) {
-            genresHash.put(genres[i], genresHash.getOrDefault(genres[i], 0) + plays[i]);
+    static class Album{
+        String genre;
+        int play;
+        int index;
+        
+        public Album(String genre, int play, int index){
+            this.genre = genre;
+            this.play = play;
+            this.index = index;
         }
-        List<String> sortGenres = sortByValue(genresHash);
+    }
+    // 속한 노래가 많이 재생된 장르 수록
+    // 장르 내에서 많이 재생된 노래 수록
+    // 장르 내에서 재생 횟수가 같은 노래면 고유번호 낮음 순서
+    public static int[] solution(String[] genres, int[] plays) {
 
-        // play 횟수 별로 정렬
-        HashMap<Integer, Integer> playsHash = new HashMap<>();
-        for (int i = 0; i < plays.length; i++) {
-            playsHash.put(i, plays[i]);
+        Map<String, Integer> genresSum = new HashMap<>();
+        for(int i = 0; i < genres.length; i++){
+            genresSum.put(genres[i], genresSum.getOrDefault(genres[i], 0) + plays[i]);
         }
-        List<Integer> sortPlays = sortByValueInt(playsHash);
 
-        // 구하기
-        for (int j = 0; j < sortGenres.size(); j++){
-            int cnt = 0;
-            for(int i : sortPlays){
-                if(genres[i].equals(sortGenres.get(j))){
-                    result.add(i);
-                    cnt++;
-                    if(cnt > 1) break;
+        List<Map.Entry<String, Integer>> sortList = new LinkedList<>(genresSum.entrySet());
+        sortList.sort(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+            return o2.getValue() - o1.getValue();
+            }
+        });
+
+        // 장르 랭킹 List
+        List<String> genresLank = new ArrayList<>();
+        for(String key : genresSum.keySet()){
+            genresLank.add(key);
+        }
+		
+        List<Album> result = new ArrayList<>();
+        for(String genre : genresLank){
+            List<Album> list = new ArrayList<>();
+            for(int i = 0; i < genres.length; i++){
+                if(genres[i].equals(genre)){
+                    list.add(new Album(genre, plays[i], i));
                 }
             }
+            list.sort(new Comparator<Album>() {
+                @Override
+                public int compare(Album o1, Album o2) {
+                       int play1 = o1.play;
+                       int play2 = o2.play;
+                       
+                       if(play1 == play2) {
+                           return o1.index - o2.index;
+                       }
+                       else if(play2 > play1) return 1;
+                       else return -1;
+                }
+            });
+         
+            result.add(list.get(0));
+            if(list.size()!= 1){
+                result.add(list.get(1));
+            }
         }
-
+        
         int[] answer = new int[result.size()];
-        for(int i = 0; i < result.size(); i++){
-            answer[i] = result.get(i);
+        for(int i=0; i<result.size(); i++){
+            answer[i] = result.get(i).index;
         }
 
         return answer;
-    }
-
-    public static List<String> sortByValue(HashMap<String, Integer> map){
-        List<String> list = new ArrayList<String>();
-        list.addAll(map.keySet());
-
-        Collections.sort(list, new Comparator<Object>(){
-            public int compare(Object o1,Object o2){
-                int v1 = map.get(o1);
-                int v2 = map.get(o2);
-
-                return v2-v1;
-            }
-        });
-        return list;
-    }
-
-    public static List<Integer> sortByValueInt(HashMap<Integer, Integer> map){
-        List<Integer> list = new ArrayList<Integer>();
-        list.addAll(map.keySet());
-
-        Collections.sort(list, new Comparator<Integer>(){
-            public int compare(Integer o1,Integer o2) {
-                int v1 = map.get(o1);
-                int v2 = map.get(o2);
-
-                if (v1 == v2) {
-                    return o1 - o2;
-                } else {
-                    return v2 - v1;
-                }
-            }
-        });
-        return list;
     }
 }
